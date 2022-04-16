e75114528bc20054bdc46e1b5fabc3a5d62045af
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-04-16 10:27:43 +0200

    Setup authentication with firebase

--- app/src/main/java/com/example/vegarden/SigninActivity.kt
+++ app/src/main/java/com/example/vegarden/SigninActivity.kt
@@ -1,16 +1,26 @@
 package com.example.vegarden
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.vegarden.databinding.ActivitySigninBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SigninActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySigninBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
         binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -19,17 +29,54 @@ class SigninActivity : AppCompatActivity() {
         }
 
         binding.fabSignin.setOnClickListener {
-            val email = binding.etEmail.text.toString()
+            val email = binding.etEmail.text.toString().trim()
             val password = binding.etPassword.text.toString()
+            var error = false
 
-            if (!email.isValidEmail())
+            if (!email.isValidEmail()) {
                 binding.tilEmail.error = "Errore"
-            else if (!password.isValidPassword())
+                error = true
+            } else{
+                binding.tilEmail.error = null
+            }
+            if (!password.isValidPassword()){
                 binding.tilPassword.error = "Errore"
-            else if (true) {
-
+                error = true
+            } else {
+                binding.tilPassword.error = null
+            }
+            if (!error) {
+                signIn(email, password)
             }
 
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    // If sign in fails, display the error messages
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    binding.tilEmail.error = "Errore"
+                    binding.tilPassword.error = "Errore"
+                }
+            }
     }
 }
--- app/src/main/java/com/example/vegarden/SignupActivity.kt
+++ app/src/main/java/com/example/vegarden/SignupActivity.kt
@@ -1,48 +1,99 @@
 package com.example.vegarden
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.vegarden.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         binding.tvSignin.setOnClickListener {
             finish()
         }
 
         binding.fabSignup.setOnClickListener {
-            if (binding.etName.text.toString().isBlank()) {
+            val name = binding.etName.text.toString()
+            val surname = binding.etSurname.text.toString()
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString()
+            val confirmPassword = binding.etConfirmPassword.text.toString()
+            var error = false
+
+            if (name.isBlank()) {
                 binding.tilName.error = "Name field should not be empty"
+                error = true
             } else {
                 binding.tilName.error = null
             }
-            if (binding.etSurname.text.toString().isBlank()) {
+            if (surname.isBlank()) {
                 binding.tilSurname.error = "Surname field should not be empty"
+                error = true
             } else {
                 binding.tilSurname.error = null
             }
-            if (!binding.etEmail.text.toString().isValidEmail()) {
+            if (!email.isValidEmail()) {
                 binding.tilEmail.error = "Email is not valid"
+                error = true
             } else {
                 binding.tilEmail.error = null
             }
-            if (!binding.etPassword.text.toString().isValidPassword()) {
+            if (!password.isValidPassword()) {
                 binding.tilPassword.error = "The password should contain at least 8 characters"
-            } else if (binding.etPassword.text.toString() != binding.etConfirmPassword.text.toString()) {
+                error = true
+            } else if (password != confirmPassword.toString()) {
                 binding.tilPassword.error = "The two passwords are different"
                 binding.tilConfirmPassword.error = "The two passwords are different"
+                error = true
             } else {
                 binding.tilPassword.error = null
                 binding.tilConfirmPassword.error = null
             }
-        }
 
+            if (!error) {
+                Toast.makeText(this, "Creating account: $email $password", Toast.LENGTH_SHORT)
+                    .show()
+                createAccount(email, password)
+            }
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    //TODO updateUI(user)
+                    Toast.makeText(
+                        baseContext, "Authentication succeded.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, task.exception.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //TODO Error updateUI(null)
+                }
+            }
     }
 }
