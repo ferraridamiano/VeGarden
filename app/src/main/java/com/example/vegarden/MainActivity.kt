df3f012d5a6f2b6cc94da2630697af16b7debe09
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-04-20 11:55:31 +0200

    Basic functions that plots the vegetable garden

--- app/src/main/java/com/example/vegarden/MainActivity.kt
+++ app/src/main/java/com/example/vegarden/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.example.vegarden
 
 import android.content.ContentValues.TAG
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.example.vegarden.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val rows = 3
+        val cols = 3
+        val garden = createVegetableGarden(this, rows, cols)
+        garden.forEach { binding.column.addView(it) }
+
 
         auth = Firebase.auth
 
@@ -35,4 +47,40 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Error getting documents.", exception)
             }
     }
+}
+
+fun createVegetableGarden(context:Context, rows: Int, columns: Int) : List<LinearLayout> {
+    val listOfRows = mutableListOf<LinearLayout>()
+    // create a list with [rows] rows and [columns] columns
+    for(i in 1..rows){
+        listOfRows.add(createRow(context, columns))
+    }
+    return listOfRows.toList()
+}
+
+fun createRow(context:Context, count: Int): LinearLayout {
+    // Create a row with an horizontal linear layout
+    val row = LinearLayout(context)
+    row.orientation = LinearLayout.HORIZONTAL
+    row.layoutParams = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+    // Then add [count] plot to the row
+    for(i in 1..count){
+        row.addView(createPlot(context))
+    }
+    return row
+}
+
+fun createPlot(context: Context): ImageView {
+    val imageView = ImageView(context)
+    imageView.setImageResource(R.drawable.plot)
+    imageView.layoutParams = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        1.0f
+    )
+    imageView.adjustViewBounds = true
+    return imageView
 }
