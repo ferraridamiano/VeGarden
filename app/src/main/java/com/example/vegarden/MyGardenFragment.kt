523ebc43b5aa69be76e8b9fbbf76cb8a97bd3d15
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-04-21 16:14:07 +0200

    Added progressbar + separator in my garden

--- app/src/main/java/com/example/vegarden/MyGardenFragment.kt
+++ app/src/main/java/com/example/vegarden/MyGardenFragment.kt
@@ -2,6 +2,7 @@ package com.example.vegarden
 
 import android.content.ContentValues
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.example.vegarden.databinding.FragmentMyGardenBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -30,7 +32,7 @@ class MyGardenFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMyGardenBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -49,21 +51,25 @@ class MyGardenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var rows = 1
-        var cols = 1
-
-        var garden = createVegetableGarden(requireContext(), rows, cols)
-        binding.column.removeAllViews()
-        garden.forEach { binding.column.addView(it) }
+        var rows = 0
+        var cols = 0
+        var garden: List<View>?
 
         db.collection("gardens").document(auth.currentUser!!.uid).get()
             .addOnSuccessListener { document ->
                 val data = document.data!!.toMap()
                 rows = data["rows"].toString().toInt()
                 cols = data["columns"].toString().toInt()
-                garden = createVegetableGarden(requireContext(), rows, cols)
+                garden = createVegetableGarden(
+                    requireContext(),
+                    rows,
+                    cols,
+                    ResourcesCompat.getDrawable(resources, R.color.plotsSeparator, null)!!,
+                    resources.getDimension(R.dimen.plotsSeparatorSize).toInt()
+                )
                 binding.column.removeAllViews()
-                garden.forEach { binding.column.addView(it) }
+                garden!!.forEach { binding.column.addView(it) }
+                binding.progressBar.visibility = View.GONE
             }
             .addOnFailureListener { exception ->
                 Log.w(ContentValues.TAG, "Error getting documents.", exception)
@@ -71,16 +77,33 @@ class MyGardenFragment : Fragment() {
     }
 }
 
-private fun createVegetableGarden(context: Context, rows: Int, columns: Int): List<LinearLayout> {
-    val listOfRows = mutableListOf<LinearLayout>()
+private fun createVegetableGarden(
+    context: Context,
+    rows: Int,
+    columns: Int,
+    separatorColor: Drawable,
+    separatorSize: Int
+): List<View> {
+    val listOfRows = mutableListOf<View>()
     // create a list with [rows] rows and [columns] columns
     for (i in 1..rows) {
-        listOfRows.add(createRow(context, columns))
+        listOfRows.add(createRow(context, columns, separatorColor, separatorSize))
+        val separator = View(context)
+        separator.layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, separatorSize)
+        separator.background = separatorColor
+        listOfRows.add(separator)
     }
+    listOfRows.removeAt(listOfRows.size - 1) // Removes latest separator
     return listOfRows.toList()
 }
 
-private fun createRow(context: Context, count: Int): LinearLayout {
+private fun createRow(
+    context: Context,
+    count: Int,
+    separatorColor: Drawable,
+    separatorSize: Int
+): LinearLayout {
     // Create a row with an horizontal linear layout
     val row = LinearLayout(context)
     row.orientation = LinearLayout.HORIZONTAL
@@ -91,7 +114,13 @@ private fun createRow(context: Context, count: Int): LinearLayout {
     // Then add [count] plot to the row
     for (i in 1..count) {
         row.addView(createPlot(context))
+        val separator = View(context)
+        separator.layoutParams =
+            LinearLayout.LayoutParams(separatorSize, LinearLayout.LayoutParams.MATCH_PARENT)
+        separator.background = separatorColor
+        row.addView(separator)
     }
+    row.removeViewAt(row.childCount - 1)  // Removes latest separator
     return row
 }
 
@@ -100,7 +129,7 @@ private fun createPlot(context: Context): ImageView {
     imageView.setImageResource(R.drawable.plot)
     imageView.layoutParams = LinearLayout.LayoutParams(
         LinearLayout.LayoutParams.WRAP_CONTENT,
-        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT,
         1.0f
     )
     imageView.adjustViewBounds = true
