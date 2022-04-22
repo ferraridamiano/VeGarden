3f80e7d0aaf4856f613da797141ce4fea4f75c47
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-04-22 11:04:34 +0200

    Adding a post now works

--- app/src/main/java/com/example/vegarden/AddPostActivity.kt
+++ app/src/main/java/com/example/vegarden/AddPostActivity.kt
@@ -2,11 +2,21 @@ package com.example.vegarden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.vegarden.databinding.ActivityAddPostBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.Calendar
 
 class AddPostActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAddPostBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +24,40 @@ class AddPostActivity : AppCompatActivity() {
         binding = ActivityAddPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+        db = Firebase.firestore
+
+        db.collection("users").document(auth.currentUser!!.uid).get()
+            .addOnSuccessListener { document ->
+                val name = document.data!!["name"]
+                val surname = document.data!!["surname"]
+                binding.tvNameSurname.text = "$name $surname"
+                binding.tvNameSurname.visibility = View.VISIBLE
+                binding.tvSays.visibility = View.VISIBLE
+            }.addOnFailureListener {
+                Toast.makeText(this, "Error connecting to internet", Toast.LENGTH_SHORT).show()
+            }
+
         binding.fabConfirm.setOnClickListener {
-            finish()
+            val text = binding.etPost.text.toString()
+            if (text.trim().isBlank()) {
+                Toast.makeText(this, "Write something or exit", Toast.LENGTH_SHORT).show()
+            } else {
+                val newPost = hashMapOf(
+                    "user" to auth.currentUser!!.uid,
+                    "type" to "post",
+                    "content" to text,
+                    "timestamp" to Calendar.getInstance().time
+                )
+                db.collection("posts").add(newPost)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Post added", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }.addOnFailureListener {
+                        Toast.makeText(this, "Error connecting to internet", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+            }
         }
 
     }
